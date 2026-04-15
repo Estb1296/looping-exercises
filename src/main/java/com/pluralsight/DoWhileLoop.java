ca06210284bfb85c997693e5387179cda569e1b9
ezrateshale <ezra.teshale12@gmail.com>
2026-04-15 09:39:33 -0500

    feat: While and do-While loop complete.

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -1,11 +1,11 @@
 package com.pluralsight;
 
-public class WhileLoop {
+public class DoWhileLoop {
     public static void main(String[] args) {
         int counter=0;
-        while(counter<5){
-            System.out.println("I love java.");
+        do{
+            System.out.println("I love Java.");
             counter++;
-        }
+        }while(counter<5);
     }
 }
